3f635ab283dd79f63eeb661a8d07192c35662f9c
Woogigooie <33742978+Woogigooie@users.noreply.github.com>
2017-11-16 22:28:32 -0700

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
