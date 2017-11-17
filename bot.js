43c7b345e688ef316099e87e40e62c5ce6eb6696
Woogigooie <33742978+Woogigooie@users.noreply.github.com>
2017-11-16 22:27:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'w!ping') {
-    	message.reply('Pong!');
-  	}
-});
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerCommandsIn(__dirname + "/commands");
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
