e893a4ee13710ff0d69fc001c1163f71f7e98972
Woogigooie <33742978+Woogigooie@users.noreply.github.com>
2017-11-16 22:39:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+client.on('message', (message) => {
+  
+  if(message.content == 'w!ping') {
+    message.reply('pong');
+  }
+  
+});
+
 bot.login('BOT_TOKEN');
